77d3b2b4c456f6398cd2826f932cb35da1a80ec2
StrummingDown <mylove0330@nate.com>
2022-03-24 21:19:16 +0900

    Update: DontSameNumber-python

--- Lv1/DontSameNumber.py
+++ Lv1/DontSameNumber.py
@@ -1,6 +1,9 @@
+# 뮨제링크
+# https://programmers.co.kr/learn/courses/30/lessons/12906#qna
+
 def solution(arr):
     answer = [] # 정답을 담을 배열
-    j = -1 # 바로 앞의 인덱스의 요소를 나타내줄 변수
+    j = -1 # 바로 앞의 인덱스의 요소를 나타내줄 변수 (입력받은 배열의 각 요소는 0 이상이기 때문에 -1로 초기값 할당)
     for i in arr: # 입력받은 배열 순회
         if i != j: # 배열의 요소가 변소 j와 다르다면, 
             j = i  # 중복 값을 판단해주기 위해 j에 i의 값 할당
