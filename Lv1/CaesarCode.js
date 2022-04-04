859757ccc622a68306f67494a81688005ba4125f
StrummingDown <mylove0330@nate.com>
2022-04-04 19:39:27 +0900

    Update: Caesar Code-js

--- Lv1/CaesarCode.js
+++ Lv1/CaesarCode.js
@@ -1,6 +1,7 @@
 // 문제링크
 // https://programmers.co.kr/learn/courses/30/lessons/12926
 
+// z에서 n만큼 이동했을때 다시 a(알파벳 문자열 처음)으로 가는걸 충분히 생각하지 않고 작성한 코드..
 // function solution(s, n) {
 //   var answer = "";
 //   let alpha = "abcdefghijklmnopqrstuvwxyz";
@@ -25,24 +26,30 @@
 //   return answer;
 // }
 
-function solution(s, n) {
-  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // 대문자 알파벳
-  var lower = "abcdefghijklmnopqrstuvwxyz"; // 소문자 알파벳
-  var answer = ""; // n만큼 움직인 알파벳을 담아줄 정답 문자열
+// z를 넘어서는 인덱스를 접근하는 방법을 인지하고 나서 다시 작성한 코드
 
-  for (var i = 0; i < s.length; i++) {
-    // 입력받은 알파벳 길이만큼 순회
-    var text = s[i]; // 인덱스로 차례차례 접근
-    if (text == " ") {
-      // 공백을 경우,
-      answer += " "; // 그대로 정답 문자열에 더해주고,
-      continue; // 밑의 실행문은 스킵한다.
+function solution(s, n) {
+  var answer = ""; // 정답을 담아줄 문자열
+  let lower = "abcdefghijklmnopqrstuvwxyz"; // 소문자 알파벳
+  let upper = lower.toUpperCase(); // 대문자 알파벳
+
+  for (let idx = 0; idx < s.length; idx++) {
+    // 입력받은 문자열 순회
+    let sequence = s[idx]; // 현재 순회중인 알파벳 할당
+    if (sequence === " ") {
+      // 공백이라면,
+      answer += " "; // 공백 그대로 정답에 더하기
+    } else {
+      // 공백이 아니라면,
+      let check = lower.includes(sequence) ? lower : upper; // 소문자인지 대문자인지 판별
+      let index = check.indexOf(sequence) + n; // n만큼 이동한 인덱스 구하기
+      if (index >= 26) {
+        // 알파벳의 총 갯수(26) Z 이상의 인덱스라먼,
+        index -= 26; // 인덱스에서 알파벳 총 갯수를 빼준다.
+      }
+      answer += check[index]; // 정답 문자열에 위에서 구한 인덱스의 알파벳 더해주기
     }
-    var textArr = upper.includes(text) ? upper : lower; // 대문자인지 소문자인지 판단
-    var index = textArr.indexOf(text) + n; // n만큼 움직인 인데스를 구해준다.
-    if (index >= textArr.length) index -= textArr.length; // 인덱스가 Z(25)를 초과한다면, 인덱스에서 전체 알파벳 길이를 뺸다.
-    answer += textArr[index]; // 정답 문자열에 n만큼 움직인 알파벳 추가
   }
   return answer; // 정답 문자열 리턴
 }
-console.log(solution("z", 1));
+console.log(solution("a B z", 1));
