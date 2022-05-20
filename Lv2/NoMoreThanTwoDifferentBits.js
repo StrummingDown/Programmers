724efdb35cbf58e3c63aaca7dabef0144d3dff35
StrummingDown <mylove0330@nate.com>
2022-05-20 17:01:31 +0900

    Update: No more than two different bits-js

--- Lv2/NoMoreThanTwoDifferentBits.js
+++ Lv2/NoMoreThanTwoDifferentBits.js
@@ -26,3 +26,19 @@ function solution(numbers) {
   for (let i of numbers) answer.push(f(i));
   return answer;
 }
+
+// 다른사람의 풀이를 참고한 코드
+
+function solution(numbers) {
+  function f(x) {
+    if (x % 2 === 0) return x + 1;
+    let bit = "0" + x.toString(2);
+    let idx = bit.lastIndexOf("0");
+    return parseInt(`${bit.slice(0, idx)}10${bit.slice(idx + 2)}`, 2);
+  }
+  const answer = [];
+  for (let number of numbers) answer.push(f(number));
+  return answer;
+}
+
+console.log(solution([2, 7]));
